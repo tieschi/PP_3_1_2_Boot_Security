b57f9a9cf091dcac69a5c2916ce3347d97619d3e
tieschi <118199545+tieschi@users.noreply.github.com>
2023-12-14 09:38:03 +0300

    Revert "task finished"
    This reverts commit ca31939439a404b944c75b5320f02912eecc58c4.

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,18 +1,19 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,34 +30,48 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
-    public String userList(Model model, Principal principal) {
+    public String userList(Model model) {
         model.addAttribute("allUsers", userService.getAllUsers());
-        model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
-        model.addAttribute("allRoles", roleService.getAllRoles());
         return "allUsers";
     }
 
-    @GetMapping("/admin/new")
-    public String newUserPage(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "new";
+    @GetMapping("/admin/addUser")
+    public String addNewUser(Model model) {
+
+        User user = new User();
+        model.addAttribute("user", user);
+
+        return "info";
     }
 
-    @RequestMapping("/admin/saveUser")
-    public String saveUser(@ModelAttribute("user")User user, @RequestParam(value = "rolessel") String[] selectRoles,
-                           @RequestParam(value = "id") int id) {
+    @RequestMapping("/admin/update")
+    public String updateUser(@RequestParam("id") int id, Model model) {
+
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+
+        return "info";
+    }
+
+    @PostMapping("/admin/saveUser")
+    public String saveUser(@Valid User user, BindingResult bindingResult,
+                           @RequestParam(required=false) String roleUser,
+                           @RequestParam(required=false) String roleAdmin) {
         Set<Role> roles = new HashSet<>();
-        for (String role: selectRoles) {
-            roles.add(roleService.findRoleByName(role));
+        if (bindingResult.hasErrors()) {
+            return "info";
+        } else {
+            if (roleUser != null && roleUser.equals("ROLE_USER")) {
+                roles.add(roleService.findRoleByName("ROLE_USER"));
+            }
+            if (roleAdmin != null && roleAdmin.equals("ROLE_ADMIN")) {
+                roles.add(roleService.findRoleByName("ROLE_ADMIN"));
+            }
+            user.setRoles(roles);
+            userService.addUser(user);
+            return "redirect:/admin";
         }
-        System.out.println(user);
-        user.setRoles(roles);
-        user.setId(id);
-        userService.addUser(user);
-        return "redirect:/admin";
     }
-
     @RequestMapping("/admin/delete")
     public String deleteUser(@RequestParam("id") int id) {
 
--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -2,9 +2,6 @@ package ru.kata.spring.boot_security.demo.service;
 
 import ru.kata.spring.boot_security.demo.model.Role;
 
-import java.util.List;
-
 public interface RoleService{
     Role findRoleByName(String name);
-    List<Role> getAllRoles();
 }
